9f3a831938d42d4cde4879866a4c139b68416271
hilmi-a <hilmi.a.aziz@gmail.com>
2015-08-27 14:32:41 +0700

    inisiasi database

--- Surat/Surat/FormLogin.cs
+++ Surat/Surat/FormLogin.cs
@@ -34,28 +34,60 @@ namespace Surat
             Database db = new Database();
             string strconn = db.getString();
             MySqlConnection conn = new MySqlConnection(strconn);
-            conn.Open();
-            string query = "SELECT * FROM user WHERE username = @username AND password = @password";
-            MySqlCommand cmd = new MySqlCommand(query, conn);
-            cmd.Parameters.AddWithValue("@username", username);
-            cmd.Parameters.AddWithValue("@password", password);
 
-            MySqlDataReader reader = cmd.ExecuteReader();
-            if (!reader.Read())
+            try
             {
-                MessageBox.Show("Username atau Password salah. Silahkan ulangi kembali.", "Kesalahan");
-                textBoxUsername.Clear();
-                textBoxPassword.Clear();
-                textBoxUsername.Focus();
+                conn.Open();
+                string query = "SELECT * FROM user WHERE username = @username AND password = @password";
+                MySqlCommand cmd = new MySqlCommand(query, conn);
+                cmd.Parameters.AddWithValue("@username", username);
+                cmd.Parameters.AddWithValue("@password", password);
+
+                MySqlDataReader reader = cmd.ExecuteReader();
+                if (!reader.Read())
+                {
+                    MessageBox.Show("Username atau Password salah. Silahkan ulangi kembali.", "Kesalahan");
+                    textBoxUsername.Clear();
+                    textBoxPassword.Clear();
+                    textBoxUsername.Focus();
+                }
+                else
+                {
+                    id_user = reader[0].ToString();
+                    this.Hide();
+                    FormMain main = new FormMain();
+                    main.Show();
+                }
+                conn.Close();
             }
-            else
+            catch (MySqlException)
             {
-                id_user = reader[0].ToString();
-                this.Hide();
-                FormMain main = new FormMain();
-                main.Show();
+                MessageBox.Show("Database tidak ditemukan atau User dan Password MySQL salah.", "Informasi", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                Database dbc = new Database();
+                string strconnect = db.getString2();
+                MySqlConnection connect = new MySqlConnection(strconnect);
+                connect.Open();
+                MySqlCommand cmd = new MySqlCommand("create database if not exists db_surat", connect);
+                MySqlCommand cmd2 = new MySqlCommand("use db_surat", connect);
+                MySqlCommand cmd3 = new MySqlCommand("CREATE TABLE IF NOT EXISTS `user` (`id_user` int(11) NOT NULL AUTO_INCREMENT,`username` varchar(19) NOT NULL,`password` varchar(10) NOT NULL,`nama` varchar(40) NOT NULL,PRIMARY KEY (`id_user`),UNIQUE KEY `username` (`username`)) ENGINE=InnoDB", connect);
+                MySqlCommand cmd4 = new MySqlCommand("insert  into `user`(`id_user`,`username`,`password`,`nama`) values (NULL,'a','a','a')", connect);
+                MySqlCommand cmd5 = new MySqlCommand("CREATE TABLE IF NOT EXISTS `bagian_bidang` (`id_bagian_bidang` int(11) NOT NULL AUTO_INCREMENT,`nama_bagian_bidang` varchar(30) DEFAULT NULL,PRIMARY KEY (`id_bagian_bidang`)) ENGINE=InnoDB", connect);
+                MySqlCommand cmd6 = new MySqlCommand("CREATE TABLE IF NOT EXISTS `jenis_surat` (`id_jenis` int(11) NOT NULL AUTO_INCREMENT,`nama_jenis` varchar(20) DEFAULT NULL,PRIMARY KEY (`id_jenis`)) ENGINE=InnoDB", connect);
+                MySqlCommand cmd7 = new MySqlCommand("CREATE TABLE IF NOT EXISTS `lampiran` (`id_lampiran` int(11) NOT NULL AUTO_INCREMENT,`nama_lampiran` varchar(40) DEFAULT NULL,PRIMARY KEY (`id_lampiran`)) ENGINE=InnoDB", connect);
+
+
+
+                cmd.ExecuteNonQuery();
+                cmd2.ExecuteNonQuery();
+                cmd3.ExecuteNonQuery();
+                cmd4.ExecuteNonQuery();
+                cmd5.ExecuteNonQuery();
+                cmd6.ExecuteNonQuery();
+                cmd7.ExecuteNonQuery();
+                //cmd8.ExecuteNonQuery();
+                connect.Close();
+                cekLogin();
             }
-            conn.Close();
         }
 
         private void buttonKeluar_Click(object sender, EventArgs e)
